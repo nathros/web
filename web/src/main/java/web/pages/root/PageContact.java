5abe816e3f21cc19d3c703f5f845ab51efde5af5
nathros <>
2020-01-23 14:59:13 +0000

    Basic form for contact page, submission not yet implemented

--- web/src/main/java/web/pages/root/PageContact.java
+++ web/src/main/java/web/pages/root/PageContact.java
@@ -14,7 +14,7 @@ public class PageContact extends BasePage {
 	@Override
 	public String getResponse() {
 		String[] css = { Resource.CSS_COMMON, Resource.CSS_HEADER, Resource.CSS_CARD, Resource.CSS_TITLE_BANNER,
-				Resource.CSS_MODAL_IMAGE, Resource.CSS_BUTTON, Resource.CSS_CAROUSEL };
+				Resource.CSS_MODAL_IMAGE, Resource.CSS_BUTTON };
 		String[] js = { Resource.JS_SNAKE_HOOK };
 
 		m.addHead(css, js, "Contact");
@@ -22,8 +22,26 @@ public class PageContact extends BasePage {
 		m.ln("<body>");
 		m.addNavbar(NavbarItem.Contact);
 
-		m.addBanner("Contact",
-				"https://www.campagnolo.com/media/immagini/9593_z_campagnolo-super-record-chain-MY2019-banner.jpg");
+		m.addBanner("Contact", "https://wallpaperplay.com/walls/full/1/b/e/366897.jpg");
+		m.ln("<div class=\"common-content\">");
+		m.ln("	<div class=\"card\">");
+
+		m.ln("<form action=\"stage/contact\" onSubmit=\"return alert('WARNING: not currently implemented');\">");
+		m.ln("	Name<br>");
+		m.ln("	<input type=\"text\" name=\"firstname\" value=\"\">");
+		m.ln("	<br><br>");
+		m.ln("	Email-address:<br>");
+		m.ln("	<input type=\"text\" name=\"email\" value=\"\">");
+		m.ln("	<br><br>");
+		m.ln("	Subject:<br>");
+		m.ln("	<input type=\"text\" name=\"subject\" value=\"\"><br><br>");
+		m.ln("	<textarea rows=\"12\" cols=\"100\" name=\"comment\"></textarea>");
+		m.ln("	<br><br>");
+		m.ln("	<input type=\"submit\" value=\"Submit\">");
+		m.ln("</form>");
+
+		m.ln("	</div>"); // card
+		m.ln("</div>"); // common-content
 
 		m.ln("</body>");
 		m.ln("</html>");
