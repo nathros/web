c59f9698851880a0c3226eb6bcac14cf8b19f3b5
nathros <>
2020-06-10 19:49:06 +0100

    Added smooth scroll to top script and made scan lines easier to configure

--- web/src/main/java/web/pages/root/PageHome.java
+++ web/src/main/java/web/pages/root/PageHome.java
@@ -27,15 +27,18 @@ public class PageHome extends BasePage {
 		m.ln("<div class=\"common-content\" style=\"margin-top:-20rem\">");
 		m.ln("	<div class=\"card\">");
 
-		m.ln("<h2>Experience</h2>");
-		m.ln("<h2>Experience</h2>");
-		m.ln("<h2>Experience</h2>");
-		m.ln("<h2>Experience</h2>");
-		m.ln("<h2>Experience</h2>");
-		m.ln("<h2>Experience</h2>");
-		m.ln("<h2>Experience</h2>");
-		m.ln("<h2>Experience</h2>");
-		m.ln("<h2>Experience</h2>");
+		for (int i = 0; i < 12; i++) {
+			m.ln("<h2>Title</h2>");
+			m.ln("<p>What is Lorem Ipsum?\r\n"
+					+ "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n"
+					+ "\r\n" + "Why do we use it?\r\n"
+					+ "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n"
+					+ "\r\n" + "\r\n" + "Where does it come from?\r\n"
+					+ "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\r\n"
+					+ "\r\n"
+					+ "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>");
+		}
+		m.ln("<a class=\"btn btn-blue ripple\" style=\"text-align: center;display:block;\" onclick=\"scrollToTop();\">Top</a>");
 
 		m.ln("	</div>"); // card
 		m.ln("</div>"); // common-content
@@ -43,6 +46,7 @@ public class PageHome extends BasePage {
 
 		m.ln("<script>");
 		m.ln(Resource.readResource(Resource.JS_BANNER));
+		m.ln(Resource.readResource(Resource.JS_SCROLL_TOP));
 		m.ln("</script>");
 
 		m.ln("</body>");
--- web/src/main/resources/web/js/banner-animation.js
+++ web/src/main/resources/web/js/banner-animation.js
@@ -1,38 +1,42 @@
-var step = 0;
+var stepFrame = 0;
 var canvas = document.getElementById("banner-canvas");
 
 var ctx = canvas.getContext("2d");
 canvas.style.width = "100%";
 canvas.style.height = "100%";
-canvas.width  = canvas.offsetWidth;
+canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
+
 var lineWidth = 4;
+var stepSize = 0.25;
+var stepFPS = 20;
 
 var fade = 0;
 var fadeDirection = true;
 var fadeMax = 0.2;
-var fadeStep = 0.01;
+var fadeStep = 0.005;
 
 function drawLine(y) {
 	ctx.beginPath();
-	ctx.strokeStyle = "rgba(0, 0, 0, " + fade + ")";
-	ctx.lineWidth = lineWidth;
-	ctx.moveTo(0, y + 0.5);
-	ctx.lineTo(canvas.width, y + 0.5);
+	ctx.moveTo(0, y);
+	ctx.lineTo(canvas.width, y);
 	ctx.stroke();
 }
 
 function bannerAnimate() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	var start = step;
-	
+	ctx.strokeStyle = "rgba(0, 0, 0, " + fade + ")";
+	ctx.lineWidth = lineWidth;
+
+	var start = stepFrame;
+
 	while (start < canvas.height) {
 		drawLine(start);
 		start += lineWidth * 2;
 	}
 	
-	step += 0.5;
-	if (step == (lineWidth * 2)) step = 0;
+	stepFrame += stepSize;
+	if (stepFrame == (lineWidth * 2)) stepFrame = 0;
 	
 	if (fadeDirection) {
 		fade += fadeStep;
@@ -45,18 +49,6 @@ function bannerAnimate() {
 			fadeDirection = !fadeDirection;
 		}
 	}
-	
-	
 }
 
-
-window.setInterval(bannerAnimate, 100);
-
-
-
-
-	
-
-
-
-
+window.setInterval(bannerAnimate, 1000 / stepFPS);
