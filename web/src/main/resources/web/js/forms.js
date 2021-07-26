1fbbac12a123a05ca20568c8442228f6e95e31ae
nathros <>
2021-07-26 11:41:14 +0100

    Form wait animation longer always runs in the background

--- web/src/main/resources/web/js/forms.js
+++ web/src/main/resources/web/js/forms.js
@@ -3,6 +3,6 @@ var formsIsolate = new function() { // Isolates this code from other JavaScript
 };
 
 function sendEmail() {
-	setTimeout(function() { document.getElementById("fullscreen-message").style.visibility = "visible"; }, 350);
+	setTimeout(function() { document.getElementById("fullscreen-message").style.display = "flex"; }, 350);
 	document.getElementById("email-form").submit();
 }
